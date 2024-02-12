6a353e58723fcdfd812344230051987a7194a129
nicomanunta <nic.manun@gmail.com>
2024-02-12 16:04:18 +0100

    aggiornamento header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-
+    @include('partials.header')
     <main>
         
     </main>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,26 @@
-<header></header>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 ">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="DC ">
+            </div>
+            <div class="col-9 d-flex align-items-center justify-content-end  ">
+                <nav class="navbar-nav container navbar-light">
+                    <ul class="list-unstyled d-flex justify-content-end">
+                        <li class=" nav-item "><a class="active" href="">CHARACTERS</a></li>
+                        <li class=" nav-item "><a class="active" href="">COMICS</a></li>
+                        <li class=" nav-item "><a class="active" href="">MOVIES</a></li>
+                        <li class=" nav-item "><a class="active" href="">TV</a></li>
+                        <li class=" nav-item "><a class="active" href="">GAMES</a></li>
+                        <li class=" nav-item "><a class="active" href="">COLLECTIBLES</a></li>
+                        <li class=" nav-item "><a class="active" href="">VIDEOS</a></li>
+                        <li class=" nav-item "><a class="active" href="">FANS</a></li>
+                        <li class=" nav-item "><a class="active" href="">NEWS</a></li>
+                        <li class=" nav-item "><a class="active" href="">SHOP</a></li>
+                 
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
