a20486cfad26d443a48e046324c6a534536de551
nicomanunta <nic.manun@gmail.com>
2024-02-13 14:39:44 +0100

    aggiunti yield extends section, aggiunta homepage

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include('partials.header')
-    <main>
+    @section('content')   
         <div class="jumbotron">
             <img class="img-jumbotron" src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
         </div>
@@ -77,9 +60,7 @@
                 </div>
             </div>
         </div>
-    </main>
-    @include('partials.footer')
+    @endsection
 
-</body>
 
-</html>
+  
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+})-> name("homepage");
+
+Route::get('/comics', function () {
     $comics = config("comics");
     return view('comics', compact('comics'));
 })-> name("comics");
